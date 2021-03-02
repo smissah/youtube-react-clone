f8a6a78e34acbc0e5f1760f82e31a63bf3bfd7dc
Stephen M <stephenmissah@outlook.com>
2021-03-02 16:00:55 +0000

    videotools

---
+++ src/Components/VideoToolbar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class VideoToolbar extends Component {
+  state = {};
+  render() {
+    return <div>video toolbar </div>;
+  }
+}
+
+export default VideoToolbar;
