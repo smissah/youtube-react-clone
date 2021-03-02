01528faed61c5681288d2adf6702649d0e49e302
stephenPC <stephenmissah@outlook.com>
2021-03-02 14:34:45 +0000

    styled aside and video list

---
+++ src/Components/AsideVideoItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Styling/Aside/SingleVidItem/SingleVidItem.css";
+const AsideVideoItem = ({
+  videoId,
+  thumbnail,
+  title,
+  channelTitle,
+  handleVideoSelect,
+}) => {
+  //   console.log(handleVideoSelect);
+  return (
+    <div className="videoItem" onClick={() => handleVideoSelect()}>
+      <img src={thumbnail} alt="" className="videoItem__thumbnail" />
+
+      <div className="videoItem__text">
+        <h4>{title}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default AsideVideoItem;
