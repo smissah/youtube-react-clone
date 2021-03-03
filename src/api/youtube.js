79f24818056b22b8389ff8a35146b42b058a052c
stephenPC <stephenmissah@outlook.com>
2021-03-03 10:55:34 +0000

    styled the scrollbar and added more search results

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
-    maxResults: 5,
+    maxResults: 10,
     key: `${process.env.REACT_APP_API_KEY}`,
   },
 });
